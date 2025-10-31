881f77740de51b60758c8271a68290ddf12c3e33
DasinduDilvan <dilwanwije@gmail.com>
2025-10-31 22:48:32 +0530

    old things removed folders

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ChatInterface from "@/components/chat-interface"
-
-export default function Home() {
-  return (
-    <main className="h-screen w-full flex flex-col bg-background">
-      <ChatInterface />
-    </main>
-  )
-}
